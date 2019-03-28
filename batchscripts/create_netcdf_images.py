469b708d027669db79632b2a4d90c613a1eb4fad
bechtold_michel <bechtold.michel@gmail.com>
2019-03-28 16:42:21 +0100

    grid bug fix

--- batchscripts/create_netcdf_images.py
+++ batchscripts/create_netcdf_images.py
@@ -6,13 +6,13 @@ from pyldas.interface import LDAS_io
 exp='SMAP_EASEv2_M36_NORTH_SCA_SMOSrw_DA'
 domain='SMAP_EASEv2_M36_NORTH'
 
-exp='SMAPL4v3_M09_PM'
-domain='SMAP_EASEv2_M09'
-
 exp='BE_M36_EASEv2_SMAPin_L4SM_v001'
 domain='SMAP_EASEv2_M36_GLOBAL'
 
 exp='SMAP_EASEv2_M09_SI_SMOSfw_DA'
+domain='SMAP_EASEv2_M09'
+
+exp='SMAPL4v3_M09_PM'
 domain='SMAP_EASEv2_M09'
 
 io = LDAS_io('daily', exp, domain)
