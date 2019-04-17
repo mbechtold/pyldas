911f5ff3f6133448f99f50c99f4e2808119949a8
mbechtold <michel.bechtold@gmx.de>
2019-04-17 13:54:54 +0200

    Update updateforkedmaster.sh

--- gitscripts/updateforkedmaster.sh
+++ gitscripts/updateforkedmaster.sh
@@ -2,7 +2,7 @@
 
 ### git remote, only when not added yet (first time)
 # git remote add upstream https://github.com/alexgruber/pyldas
-#git remote -v
+# git remote -v
 # (1) update forked master
 git checkout master
 git fetch upstream
